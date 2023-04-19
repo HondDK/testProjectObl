e5a3233721b7df6921f808bbc2c0f1545bc3d54a
HondDK <Honddk99@gmail.com>
2023-04-19 15:54:15 +0600

    add-q

--- src/components/testPage/QComparisonQuestions.jsx
+++ src/components/testPage/QComparisonQuestions.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetchData from "../../hooks/useFetchData";
-
 import CardComparisonQuestions from "../UI/CardComparisonQuestions";
+
 const QComparisonQuestions = () => {
 	const data = useFetchData(
 		"http://165.232.118.51:8000/edu_exams/exams/exams/1feba260-81ca-4003-96a6-a12353c94e32/"
 	);
+
+	const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+	const handleAnswerClick = (answer) => {
+		setSelectedAnswer(answer);
+	};
 
 	return (
 		<>
@@ -14,13 +20,23 @@ const QComparisonQuestions = () => {
 				data.comparison_questions.map((item) => (
 					<section className="q_one_answer" key={item.uuid}>
 						<p>{item.header}</p>
-						<div className="card_comparison_questions">
-							{item.option_answers.map((answer) => (
+
+						{item.option_answers.map((answer) => (
+							<div
+								className="card_comparison_questions"
+								onClick={() => handleAnswerClick(answer)}
+								style={{
+									backgroundColor:
+										selectedAnswer && selectedAnswer.uuid === answer.uuid
+											? "red"
+											: "white",
+								}}
+							>
 								<CardComparisonQuestions key={answer.uuid}>
 									{answer.text}
 								</CardComparisonQuestions>
-							))}
-						</div>
+							</div>
+						))}
 					</section>
 				))}
 		</>
