b33d21ed34d3b0d4f0887f5021aefed51ee84194
HondDK <Honddk99@gmail.com>
2023-04-20 22:23:17 +0600

    timer

--- src/components/testPage/Timer.jsx
+++ src/components/testPage/Timer.jsx
@@ -1,11 +1,55 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
-const Timer = () => {
-	
+const Timer = ({ hours, minutes, seconds }) => {
+	const [timeLeft, setTimeLeft] = useState({
+		hours: parseInt(hours),
+		minutes: parseInt(minutes),
+		seconds: parseInt(seconds),
+	});
+
+	useEffect(() => {
+		const timer =
+			timeLeft.hours > 0 || timeLeft.minutes > 0 || timeLeft.seconds > 0
+				? setTimeout(() => {
+						if (timeLeft.seconds === 0) {
+							if (timeLeft.minutes === 0) {
+								if (timeLeft.hours === 0) {
+									clearTimeout(timer);
+								} else {
+									setTimeLeft({
+										hours: timeLeft.hours - 1,
+										minutes: 59,
+										seconds: 59,
+									});
+								}
+							} else {
+								setTimeLeft({
+									hours: timeLeft.hours,
+									minutes: timeLeft.minutes - 1,
+									seconds: 59,
+								});
+							}
+						} else {
+							setTimeLeft({
+								hours: timeLeft.hours,
+								minutes: timeLeft.minutes,
+								seconds: timeLeft.seconds - 1,
+							});
+						}
+				  }, 1000)
+				: undefined;
+
+		return () => clearTimeout(timer);
+	}, [timeLeft]);
 
 	return (
 		<div>
-			<span className="timer">30:00</span>
+			<span className="timer">
+				{timeLeft.hours.toString().padStart(2, "0")}:
+				{timeLeft.minutes.toString().padStart(2, "0")}:
+				{timeLeft.seconds.toString().padStart(2, "0")}
+			</span>
 		</div>
 	);
 };
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -12,18 +12,34 @@ const TestPage = () => {
 	const data = useFetchData(
 		"http://165.232.118.51:8000/edu_exams/exams/exams/1feba260-81ca-4003-96a6-a12353c94e32"
 	);
-
+	const [timeToPass, setTimeToPass] = useState({
+		hours_to_pass: 1,
+		minutes_to_pass: 0,
+		seconds_to_pass: 0,
+	});
+	useEffect(() => {
+		setTimeToPass({
+			hours_to_pass: data.hours_to_pass,
+			minutes_to_pass: data.minutes_to_pass,
+			seconds_to_pass: data.seconds_to_pass,
+		});
+	}, []);
 	return (
 		<>
 			<header>
 				<h1>{data.name}</h1>
-				<Timer />
+				<Timer
+					hours={timeToPass.hours_to_pass}
+					minutes={timeToPass.minutes_to_pass}
+					seconds={timeToPass.seconds_to_pass}
+				/>
 			</header>
 			{/* <Nav props={data} /> */}
 			<main>
-				{/* <QComparisonQuestions></QComparisonQuestions> */}
-				{/* <QTable></QTable> */}
-				{/* <QInputAnswer></QInputAnswer> */}
+				<QComparisonQuestions></QComparisonQuestions>
+				<QTable></QTable>
+
+				<QInputAnswer></QInputAnswer>
 				<QOneAnswer></QOneAnswer>
 			</main>
 		</>
