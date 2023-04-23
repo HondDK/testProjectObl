d37a899e43501b120dd2b1bb3b2875142b21f4a2
HondDK <Honddk99@gmail.com>
2023-04-23 22:21:36 +0600

    disableButton

--- src/components/testPage/QInputAnswer.jsx
+++ src/components/testPage/QInputAnswer.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import useFetchData from "../../hooks/useFetchData";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 const QInputAnswer = (props) => {
 	const [answerId, setAnswerId] = useState();
-	const [answer, setAnswer] = useState("");
+	const [answer, setAnswer] = useState([]);
 
 	const data = useFetchData(
 		"http://165.232.118.51:8000/edu_exams/exams/exams/1feba260-81ca-4003-96a6-a12353c94e32/"
 	);
 
-	function submit() {
+	const [buttonDisabled, setButtonDisabled] = useState([]);
+	function submit(index) {
 		const article = {
 			student_exam: props.uuid,
 			question: "f05e5d11-35bc-4a38-8c8f-4eba2f3a1f96",
@@ -23,19 +25,27 @@ const QInputAnswer = (props) => {
 			)
 			.then((response) => setAnswerId(response.data.id));
 		console.log(article);
+		const newButtonDisabled = [...buttonDisabled];
+		newButtonDisabled[index] = true;
+		setButtonDisabled(newButtonDisabled);
 	}
 
 	return (
 		<>
 			{data &&
 				data.original_questions &&
-				data.original_questions.map((item) => (
+				data.original_questions.map((item, index) => (
 					<section className="q_input_answer">
 						<p>{item.header}</p>
 						<span className="description">{item.description}</span>
 						<div className="q_input_answer_block">
 							<input onChange={(e) => setAnswer(e.target.value)} />
-							<button onClick={submit}>Готово</button>
+							<button
+								disabled={buttonDisabled[index]}
+								onClick={() => submit(index)}
+							>
+								Готово
+							</button>
 						</div>
 					</section>
 				))}
